05ad2dea1b91831e654819bcee875a6d4f926f4f
vorsakha <andreitf.dev@gmail.com>
2021-08-23 17:57:19 -0300

    add Footer component

---
+++ src/components/Footer/Footer.styled.ts
@@ -0,0 +1,14 @@
+import styled from "styled-components";
+
+export const FooterContainer = styled.footer`
+  height: 60px;
+  display: flex;
+  align-items: center;
+  flex-direction: row;
+  justify-content: space-between;
+  align-self: center;
+  width: 100%;
+  font-size: 0.8rem;
+  padding: 1rem;
+  color: #999;
+`;
---
+++ src/components/Footer/index.tsx
@@ -0,0 +1,27 @@
+import { Link } from "react-router-dom";
+import Row from "../common/Row";
+import Wrapper from "../common/Wrapper";
+import { FooterContainer } from "./Footer.styled";
+
+const Footer = () => {
+  const year = new Date().getFullYear();
+
+  return (
+    <Wrapper>
+      <FooterContainer>
+        <Row>
+          <Link to="/about">About</Link>
+        </Row>
+        <Row>
+          <p>©{year}</p>
+          <a href="https://github.com/vorsakha/" target="__blank">
+            {" "}
+            @vorsakha
+          </a>
+        </Row>
+      </FooterContainer>
+    </Wrapper>
+  );
+};
+
+export default Footer;
