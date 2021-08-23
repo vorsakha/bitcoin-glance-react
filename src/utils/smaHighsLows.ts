055a0689ec6ba1f0a2be852262201ca664b31109
vorsakha <andreitf.dev@gmail.com>
2021-08-23 17:48:05 -0300

    add high/low ultility

---
+++ src/utils/smaHighsLows.ts
@@ -0,0 +1,13 @@
+const getHL = (high: any, low: any, periods: number, start = 1): number => {
+  const sliceHigh = high.slice(start, periods + 1);
+  const sliceLow = low.slice(start, periods + 1);
+
+  const highNumber = Math.max(...sliceHigh);
+  const lowNumber = Math.min(...sliceLow);
+
+  const result = (highNumber + lowNumber) / 2;
+
+  return result;
+};
+
+export default getHL;
