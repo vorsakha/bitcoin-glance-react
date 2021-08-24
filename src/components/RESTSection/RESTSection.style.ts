84576da42834ea0cd629f6757afd5af63a3ee7a3
vorsakha <andreitf.dev@gmail.com>
2021-08-24 11:01:10 -0300

    RESTSection - add rest hook and use data

---
+++ src/components/RESTSection/RESTSection.style.ts
@@ -0,0 +1,11 @@
+import styled from "styled-components";
+
+export const Glance = styled.strong`
+  font-weight: 700;
+  font-size: 1.4rem;
+
+  @media screen and (min-width: 768px) {
+    font-weight: 700;
+    font-size: 1.7rem;
+  }
+`;
--- src/components/RESTSection/index.tsx
+++ src/components/RESTSection/index.tsx
@@ -1,8 +1,37 @@
-const RESTSection = () => {
+import useRest from "../../hooks/useRest";
+import { SpinnerScoped } from "../common/Spinner";
+import { Title, TitleH3 } from "../common/Title";
+import { WrapperMarginY } from "../common/Wrapper";
+import { Glance } from "./RESTSection.style";
+
+const RESTSection = ({ currency }: { currency: string }) => {
+  const four = useRest(currency);
+  const day = useRest(currency, 240, "1d");
+  const week = useRest(currency, 240, "1w");
+
   return (
-    <div>
-      <h1>REST</h1>
-    </div>
+    <WrapperMarginY>
+      <Title>Market Glance:</Title>
+
+      <TitleH3>
+        Short Term{" - "}
+        <Glance style={{ color: four.signalObj?.color }}>
+          {four.loading ? <SpinnerScoped /> : four.signalObj?.signal}
+        </Glance>
+      </TitleH3>
+      <TitleH3>
+        Medium Term{" - "}
+        <Glance style={{ color: day.signalObj?.color }}>
+          {day.loading ? <SpinnerScoped /> : day.signalObj?.signal}
+        </Glance>
+      </TitleH3>
+      <TitleH3>
+        Long Term{" - "}
+        <Glance style={{ color: week.signalObj?.color }}>
+          {week.loading ? <SpinnerScoped /> : week.signalObj?.signal}
+        </Glance>
+      </TitleH3>
+    </WrapperMarginY>
   );
 };
 
