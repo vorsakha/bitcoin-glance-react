05f500737a01cbd72d11024790f749bb91ad2f0c
vorsakha <andreitf.dev@gmail.com>
2021-08-23 21:22:29 -0300

    Layout - min-height change

--- src/components/Layout/Layout.styles.ts
+++ src/components/Layout/Layout.styles.ts
@@ -1,6 +1,6 @@
 import styled from "styled-components";
 
 export const Container = styled.div`
-  min-height: calc(100vh - 220px);
+  min-height: calc(100vh - 236px);
   padding: 1rem;
 `;
--- src/components/Layout/index.tsx
+++ src/components/Layout/index.tsx
@@ -1,5 +1,5 @@
 import GlobalStyles from "../../styles/GlobalStyles";
-import Wrapper from "../common/Wrapper";
+import { Wrapper } from "../common/Wrapper";
 import Footer from "../Footer";
 import Header from "../Header";
 import { Container } from "./Layout.styles";
