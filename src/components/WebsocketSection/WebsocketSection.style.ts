650bd8378870c8b4488a1988e4e44fdd8c19e3cc
vorsakha <andreitf.dev@gmail.com>
2021-08-24 11:00:13 -0300

    WebsocketSection - using formatted currency

--- src/components/WebsocketSection/WebsocketSection.style.ts
+++ src/components/WebsocketSection/WebsocketSection.style.ts
@@ -3,4 +3,9 @@ import styled from "styled-components";
 export const Price = styled.strong`
   padding-left: 10px;
   font-weight: 700;
+  font-size: 1.5rem;
+
+  @media screen and (min-width: 768px) {
+    font-size: 2rem;
+  }
 `;
--- src/components/WebsocketSection/index.tsx
+++ src/components/WebsocketSection/index.tsx
@@ -1,4 +1,5 @@
 import useWebsocket from "../../hooks/useWebsocket";
+import formatCurrency from "../../utils/formatCurrency";
 import { SpinnerScoped } from "../common/Spinner";
 import { Title } from "../common/Title";
 import { Price } from "./WebsocketSection.style";
@@ -7,12 +8,18 @@ const WebsocketSection = ({ currency }: { currency: string }) => {
   const websocket = useWebsocket(currency);
 
   return (
-    <Title>
-      Price Now:{" "}
-      <Price style={{ color: websocket.color }}>
-        {websocket.price === 0 ? <SpinnerScoped /> : websocket.price}
-      </Price>
-    </Title>
+    <>
+      <Title>
+        Updated Price:{" "}
+        <Price style={{ color: websocket.color }}>
+          {websocket.price === 0 ? (
+            <SpinnerScoped />
+          ) : (
+            formatCurrency(currency, websocket.price)
+          )}
+        </Price>
+      </Title>
+    </>
   );
 };
 
