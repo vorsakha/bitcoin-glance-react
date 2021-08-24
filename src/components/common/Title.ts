7a46bb144f331bbc3fe1b662b644fb4541573db7
vorsakha <andreitf.dev@gmail.com>
2021-08-24 10:52:56 -0300

    title - responsive

--- src/components/common/Title.ts
+++ src/components/common/Title.ts
@@ -3,8 +3,20 @@ import styled from "styled-components";
 export const Title = styled.h2`
   font-family: "Ubuntu", sans-serif;
   font-weight: 700;
+  font-size: 1.4rem;
+  padding: 0.5rem 0;
+
+  @media screen and (min-width: 768px) {
+    font-size: 1.7rem;
+    /* padding: 1rem 0; */
+  }
 `;
 export const TitleH3 = styled.h3`
   font-family: "Ubuntu", sans-serif;
-  font-weight: 700;
+  font-weight: 300;
+  font-size: 1.1rem;
+
+  @media screen and (min-width: 768px) {
+    font-size: 1.4rem;
+  }
 `;
