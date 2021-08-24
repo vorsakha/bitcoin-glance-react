68509a9ab51bb5dc56eb39eac5a08cda4cd21883
vorsakha <andreitf.dev@gmail.com>
2021-08-24 11:02:22 -0300

    Layout - add navigation to layout/ minheight update

--- src/components/Layout/Layout.styles.ts
+++ src/components/Layout/Layout.styles.ts
@@ -1,6 +1,10 @@
 import styled from "styled-components";
 
 export const Container = styled.div`
-  min-height: calc(100vh - 236px);
+  min-height: calc(100vh - 292px);
   padding: 1rem;
+
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
 `;
--- src/components/Layout/index.tsx
+++ src/components/Layout/index.tsx
@@ -2,12 +2,14 @@ import GlobalStyles from "../../styles/GlobalStyles";
 import { Wrapper } from "../common/Wrapper";
 import Footer from "../Footer";
 import Header from "../Header";
+import Navigation from "../Navigation";
 import { Container } from "./Layout.styles";
 
 const Layout: React.FC = ({ children }) => {
   return (
     <Wrapper>
       <Header />
+      <Navigation />
       <Container>
         <GlobalStyles />
         {children}
