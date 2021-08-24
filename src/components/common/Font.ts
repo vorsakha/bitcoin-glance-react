005d0b0efded66e93a3b0079345fd972057bd455
vorsakha <andreitf.dev@gmail.com>
2021-08-24 17:18:21 -0300

    add font styled component

---
+++ src/components/common/Font.ts
@@ -0,0 +1,11 @@
+import styled from "styled-components";
+
+export const Space = styled.span`
+  font-family: "Space Mono", monospace;
+  font-size: 1.7rem;
+  font-weight: 700;
+`;
+export const Ubuntu = styled.span`
+  font-family: "Ubuntu", sans-serif;
+  font-weight: 700;
+`;
