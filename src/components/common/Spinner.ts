b7c01d5762c3aff168cfd229f84c200ba9d24bf7
vorsakha <andreitf.dev@gmail.com>
2021-08-24 10:53:21 -0300

    spinner - change icon

--- src/components/common/Spinner.ts
+++ src/components/common/Spinner.ts
@@ -1,5 +1,5 @@
 import styled, { keyframes } from "styled-components";
-import { ImSpinner8 } from "@react-icons/all-files/im/ImSpinner8";
+import { ImSpinner2 } from "@react-icons/all-files/im/ImSpinner2";
 
 const spin = keyframes`
     100% {
@@ -7,12 +7,12 @@ const spin = keyframes`
     }
     
 `;
-export const SpinnerScoped = styled(ImSpinner8)`
+export const SpinnerScoped = styled(ImSpinner2)`
   color: #ff9900;
   font-size: 1rem;
   animation: ${spin} 1s linear infinite;
 `;
-export const Spinner = styled(ImSpinner8)`
+export const Spinner = styled(ImSpinner2)`
   position: absolute;
   top: 45%;
   left: 45%;
