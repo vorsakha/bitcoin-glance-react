6454a56efffea49c0c32437bedca0f3cf49313c5
vorsakha <andreitf.dev@gmail.com>
2021-08-24 17:18:55 -0300

    add glace icons component

---
+++ src/components/common/GlanceIcons.ts
@@ -0,0 +1,14 @@
+import styled from "styled-components";
+import { FcBullish } from "@react-icons/all-files/fc/FcBullish";
+import { FcBearish } from "@react-icons/all-files/fc/FcBearish";
+import { AiOutlineSmallDash } from "@react-icons/all-files/ai/AiOutlineSmallDash";
+
+export const BullishIcon = styled(FcBullish)`
+  font-size: 3rem;
+`;
+export const BearishIcon = styled(FcBearish)`
+  font-size: 3rem;
+`;
+export const StableIcon = styled(AiOutlineSmallDash)`
+  font-size: 3rem;
+`;
