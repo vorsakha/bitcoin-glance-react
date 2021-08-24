1c2b371207c870e71d5a29c2835f5789146d13e0
vorsakha <andreitf.dev@gmail.com>
2021-08-23 21:19:51 -0300

    Title - common h2 and h3 titles

---
+++ src/components/common/Title.ts
@@ -0,0 +1,10 @@
+import styled from "styled-components";
+
+export const Title = styled.h2`
+  font-family: "Ubuntu", sans-serif;
+  font-weight: 700;
+`;
+export const TitleH3 = styled.h3`
+  font-family: "Ubuntu", sans-serif;
+  font-weight: 700;
+`;
