f5a3d5a4a08570d5fe8aa10e8a8c53b2027c5e21
vorsakha <andreitf.dev@gmail.com>
2021-08-23 21:20:16 -0300

    Wrapper - Banner wrapper

--- src/components/common/Wrapper.ts
+++ src/components/common/Wrapper.ts
@@ -1,8 +1,13 @@
 import styled from "styled-components";
 
-const Wrapper = styled.div`
-  max-width: 1100px;
+export const Wrapper = styled.div`
+  max-width: 800px;
   margin: 0 auto;
 `;
-
-export default Wrapper;
+export const Banner = styled.div`
+  width: 100%;
+  margin: 0 0 1rem 0;
+  display: flex;
+  flex-direction: row;
+  justify-content: center;
+`;
