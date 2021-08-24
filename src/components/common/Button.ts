4504b81cdead655fb8901d039a227dedb23930bf
vorsakha <andreitf.dev@gmail.com>
2021-08-23 21:19:06 -0300

    Button  - common ButtonLink

---
+++ src/components/common/Button.ts
@@ -0,0 +1,18 @@
+import { Link } from "react-router-dom";
+import styled from "styled-components";
+
+export const ButtonLink = styled(Link)`
+  background-color: #ff9900;
+  color: #fff;
+  padding: 0.5rem 2rem;
+  margin: 0 1rem;
+  border-radius: 4px;
+  text-decoration: none;
+  font-size: 1.2rem;
+  font-weight: 600;
+
+  :hover {
+    background-color: #ca7900;
+    color: #fff;
+  }
+`;
