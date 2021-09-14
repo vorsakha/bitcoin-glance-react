bcf7e0f9ed59b9ece6b021f5128e82748215acc3
vorsakha <andreitf.dev@gmail.com>
2021-09-14 10:50:22 -0300

    RESTSection - strongly type currency prop /fix/

--- src/components/RESTSection/index.tsx
+++ src/components/RESTSection/index.tsx
@@ -6,7 +6,7 @@ import { Title } from "../common/Title";
 import { WrapperMarginY } from "../common/Wrapper";
 import { GlanceWrapper } from "./RESTSection.style";
 
-const RESTSection = ({ currency }: { currency: "brl" | "usd" }) => {
+const RESTSection = ({ currency }: { currency: "BTCBRL" | "BTCUSDT" }) => {
   const four = useRest(currency);
   const day = useRest(currency, 240, "1d");
   const week = useRest(currency, 240, "1w");
