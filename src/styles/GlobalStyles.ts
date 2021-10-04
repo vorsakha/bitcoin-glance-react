6b983db67f31f31aed7cab3cf5eed24def84489d
vorsakha <andreitf.dev@gmail.com>
2021-10-04 15:37:15 -0300

    resolving styled components warning about importing font

--- src/styles/GlobalStyles.ts
+++ src/styles/GlobalStyles.ts
@@ -1,7 +1,6 @@
 import { createGlobalStyle } from "styled-components";
 
 const GlobalStyles = createGlobalStyle`
-@import url('https://fonts.googleapis.com/css2?family=Ubuntu:wght@400;700&display=swap');
     * {
         box-sizing: border-box;
         margin: 0;
