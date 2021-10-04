d5b3a8a8c01f9410ac4767b6760ac18a1c279947
vorsakha <andreitf.dev@gmail.com>
2021-10-04 15:38:33 -0300

    adding useEffect exh deps

--- src/hooks/useRest.tsx
+++ src/hooks/useRest.tsx
@@ -44,111 +44,107 @@ const useRest = (
     }
   };
 
-  const apiCall = async () => {
-    setLoading(true);
-
-    const upperCaseSym = symbol.toUpperCase();
-    const call: any = await publicCall("/v1/klines", {
-      symbol: upperCaseSym,
-      limit,
-      interval,
-    });
-
-    const open: string[] = [];
-    const high: string[] = [];
-    const low: string[] = [];
-    const close: string[] = [];
-
-    call?.forEach((data: string[]) => {
-      open.unshift(data[1]);
-      high.unshift(data[2]);
-      low.unshift(data[3]);
-      close.unshift(data[4]);
-    });
-
-    if (open.length !== 0) {
-      setRestObj({
-        open: open,
-        high: high,
-        low: low,
-        close: close,
-      });
-
-      setLoading(false);
-    }
-  };
-
-  // Glance status from ichimoku analysis
-  // eslint-disable-next-line react-hooks/exhaustive-deps
-  const getSignal = () => {
-    try {
-      const kumo = getIchimoku({
-        close: restObj.close,
-        high: restObj.high,
-        low: restObj.low,
-      });
-
-      // Signal parameters
-      const bullish =
-        kumo.tenkan > kumo.kijun &&
-        kumo.spanAFuture > kumo.spanBFuture &&
-        kumo.price > kumo.spanAPast &&
-        kumo.price > kumo.spanBPast &&
-        kumo.chikou === "BULL";
-
-      const bearish =
-        kumo.tenkan < kumo.kijun &&
-        kumo.price < kumo.spanBPast &&
-        kumo.price < kumo.spanAPast &&
-        kumo.chikou === "BEAR";
-
-      // Return parameters results
-      if (bullish) {
-        setSignal({
-          signal: "Bullish Trend",
-          color: "#37BC34",
-          icon: BullishIcon,
-          border: "rgba(55, 188, 52, 0.3)",
-        });
-      } else if (bearish) {
-        setSignal({
-          signal: "Bearish Trend",
-          color: "#BC345B",
-          icon: BearishIcon,
-          border: "rgba(188, 52, 91, 0.3)",
-        });
-      } else {
-        setSignal({
-          signal: `Neutral Trend`,
-          color: "#333",
-          icon: StableIcon,
-          border: "rgba(99, 99, 99, 0.3)",
-        });
-      }
-
-      return {
-        bull: bullish,
-        bear: bearish,
-      };
-    } catch (error) {
-      console.log(error);
-    }
-  };
-
   useEffect(() => {
+    const apiCall = async () => {
+      setLoading(true);
+
+      const upperCaseSym = symbol.toUpperCase();
+      const call: any = await publicCall("/v1/klines", {
+        symbol: upperCaseSym,
+        limit,
+        interval,
+      });
+
+      const open: string[] = [];
+      const high: string[] = [];
+      const low: string[] = [];
+      const close: string[] = [];
+
+      call?.forEach((data: string[]) => {
+        open.unshift(data[1]);
+        high.unshift(data[2]);
+        low.unshift(data[3]);
+        close.unshift(data[4]);
+      });
+
+      if (open.length !== 0) {
+        setRestObj({
+          open: open,
+          high: high,
+          low: low,
+          close: close,
+        });
+
+        setLoading(false);
+      }
+    };
+
     async function asyncCall() {
       await apiCall();
     }
 
     asyncCall();
-    // eslint-disable-next-line react-hooks/exhaustive-deps
-  }, []);
+  }, [interval, limit, symbol]);
 
   useEffect(() => {
-    getSignal();
+    // Glance status from ichimoku analysis
+    const getSignal = () => {
+      try {
+        const kumo = getIchimoku({
+          close: restObj.close,
+          high: restObj.high,
+          low: restObj.low,
+        });
 
-    // eslint-disable-next-line react-hooks/exhaustive-deps
-  }, [restObj]);
+        // Signal parameters
+        const bullish =
+          kumo.tenkan > kumo.kijun &&
+          kumo.spanAFuture > kumo.spanBFuture &&
+          kumo.price > kumo.spanAPast &&
+          kumo.price > kumo.spanBPast &&
+          kumo.chikou === "BULL";
+
+        const bearish =
+          kumo.tenkan < kumo.kijun &&
+          kumo.price < kumo.spanBPast &&
+          kumo.price < kumo.spanAPast &&
+          kumo.chikou === "BEAR";
+
+        // Return parameters results
+        if (bullish) {
+          setSignal({
+            signal: "Bullish Trend",
+            color: "#37BC34",
+            icon: BullishIcon,
+            border: "rgba(55, 188, 52, 0.3)",
+          });
+        } else if (bearish) {
+          setSignal({
+            signal: "Bearish Trend",
+            color: "#BC345B",
+            icon: BearishIcon,
+            border: "rgba(188, 52, 91, 0.3)",
+          });
+        } else {
+          setSignal({
+            signal: `Neutral Trend`,
+            color: "#333",
+            icon: StableIcon,
+            border: "rgba(99, 99, 99, 0.3)",
+          });
+        }
+
+        return {
+          bull: bullish,
+          bear: bearish,
+        };
+      } catch (error) {
+        console.log(error);
+      }
+    };
+
+    getSignal();
+  }, [restObj.close, restObj.high, restObj.low]);
 
   return { signalObj, loading };
 };
